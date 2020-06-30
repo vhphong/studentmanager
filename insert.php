2e44f994c2a98cd6d3017d9a29b2623764a7d713
Phong Vo <vhphong>
2020-06-30 11:48:28 -0400

    added edit.php and delete.php

--- insert.php
+++ insert.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
   $dbconnection = mysqli_connect("localhost", "root", "", "dbstudentmanager");
 
@@ -22,48 +21,7 @@
       $stdSubmissions = $_POST['stdsubmission'];
 
       $queryInsert = "INSERT INTO studentinfo(sid, firstname, lastname, ssn, dob, gender, race, photo, submission)
-                VALUES ('', '$stdFName', '$stdLName', '$stdSSN', '$stdDOB', '$stdGender', '$stdRace', '$stdAvatar', '$stdSubmissions')";
-
-      $result = mysqli_query($dbconnection, $queryInsert);
-
-      if ($result) {
-        header("location:view.php");
-      }
-      else {
-        echo "Please check the query";
-      }
-    }
-  }
-  else {
-    // header("location:index.php");
-    die("Please fill in all blanks");
-  }
-?>
-=======
-<?php
-  $dbconnection = mysqli_connect("localhost", "root", "", "dbstudentmanager");
-
-  // if Insert button was pressed
-  if (isset($_POST['insert']))
-  {
-    // check for null values
-    if (empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['birthday']) || empty($_POST['stdgender']) || empty($_POST['stdrace'])) {
-      // die("Please fill in First Name, Last Name, DOB, Gender and Race");
-      header("location:index.php");
-    }
-    else {
-      $stdID = $_POST['stdid'];
-      $stdFName = $_POST['fname'];
-      $stdLName = $_POST['lname'];
-      $stdSSN = $_POST['ss'];
-      $stdDOB = $_POST['birthday'];
-      $stdGender = $_POST['stdgender'];
-      $stdRace = $_POST['stdrace'];
-      $stdAvatar = $_POST['avatar'];
-      $stdSubmissions = $_POST['stdsubmission'];
-
-      $queryInsert = "INSERT INTO studentinfo(sid, firstname, lastname, ssn, dob, gender, race, photo, submission)
-                VALUES ('', '$stdFName', '$stdLName', '$stdSSN', '$stdDOB', '$stdGender', '$stdRace', '$stdAvatar', '$stdSubmissions')";
+                      VALUES ('', '$stdFName', '$stdLName', '$stdSSN', '$stdDOB', '$stdGender', '$stdRace', '$stdAvatar', '$stdSubmissions')";
 
       $result = mysqli_query($dbconnection, $queryInsert);
 
