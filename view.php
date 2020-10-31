612e90190cfc0762a41e29694f14adbd8b834ce9
vhphong <36240749+vhphong@users.noreply.github.com>
2020-10-31 12:02:27 -0400

    added: confirm to delete records

--- view.php
+++ view.php
@@ -9,6 +9,24 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <style>
+      #students table, th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: center;
+        background-color: #4CAF50;
+        color: purple;
+        border: 1px solid Black;
+        border-collapse: collapse;
+      }
+      table, th, td {
+        border: 1px solid Black;
+        border-collapse: collapse;
+      }
+      tr {
+        text-align: center;
+      }
+    </style>
     <meta charset="utf-8">
     <title>Student Manager Records</title>
   </head>
@@ -18,48 +36,72 @@
       <h4>view.php</h4>
     </center>
     <hr width=100%>
-    <a href="index.php">Back to Data Entry Form</a>
+    <a href="index.php">Back to Main Data Entry Form index.php</a>
+    <br>
     <br><br>
-    <table>
+    <table id="students">
       <tr>
-        <td>Student ID</td>
-        <td>First Name</td>
-        <td>Last Name</td>
-        <td>SSN</td>
-        <td>Date of birth</td>
-        <td>Gender</td>
-        <td>Race</td>
-        <td>Avatar</td>
-        <td>Submissions</td>
+        <th>Student ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>SSN</th>
+        <th>Date of birth <br>(yyyy-mm-dd)</th>
+        <th>Gender</th>
+        <th>Race</th>
+        <th>Avatar</th>
+        <th>Submissions</th>
+        <th></th>
+        <th></th>
+        <th></th>
       </tr>
 
       <?php
         while ($row=mysqli_fetch_assoc($resultView)) {
-          $stdID = $row['sid'];
-          $stdFName = $row['firstname'];
-          $stdLName = $row['lastname'];
-          $stdSSN = $row['ssn'];
-          $stdDOB = $row['dob'];
-          $stdGender = $row['gender'];
-          $stdRace = $row['race'];
-          $stdAvatar = $row['photo'];
-          $stdSubmissions = $row['submission'];
-      ?>
-        <tr>
-          <td><?php echo $stdID ?></td>
-          <td><?php echo $stdFName ?></td>
-          <td><?php echo $stdLName ?></td>
-          <td><?php echo $stdSSN ?></td>
-          <td><?php echo $stdDOB ?></td>
-          <td><?php echo $stdGender ?></td>
-          <td><?php echo $stdRace ?></td>
-          <td><?php echo $stdAvatar ?></td>
-          <td><?php echo $stdSubmissions ?></td>
-          <td><a href="edit.php?editID=<?php echo $stdID ?>">Edit</a></td>
-          <td><a href="delete.php?deleteID=<?php echo $stdID ?>">Delete</a></td>
-        </tr>
-        <?php
-      } // end of while
+            $stdID = $row['sid'];
+            $stdFName = $row['firstname'];
+            $stdLName = $row['lastname'];
+            $stdSSN = $row['ssn'];
+            $stdDOB = $row['dob'];
+            $stdGender = $row['gender'];
+            $stdRace = $row['race'];
+            $stdAvatar = $row['photo'];
+            $stdSubmissions = $row['submission'];
+          ?>
+          <tr>
+            <td><?php echo $stdID ?></td>
+            <td><?php echo $stdFName ?></td>
+            <td><?php echo $stdLName ?></td>
+            <td><?php echo $stdSSN ?></td>
+            <td><?php echo $stdDOB ?></td>
+            <td><?php echo $stdGender ?></td>
+            <td><?php echo $stdRace ?></td>
+            <td><?php echo $stdAvatar ?></td>
+            <td><?php
+                   // Open 'avatar' directory, and read its contents
+                   $dir = "C:\\xampp\\htdocs\\PHP_Practice\\studentmanager\\" . $stdLName . '_' . $stdFName;
+                   if (is_dir($dir)){
+                     if ($dh = opendir($dir)){
+                       while (($file = readdir($dh)) !== false){
+                         echo $file . "<br>";
+                       }
+                       // close the directory
+                       closedir($dh);
+                     }
+                   }
+                ?>
+            </td>
+            <td><a href="edit.php?editID=<?php echo $stdID ?>">Edit</a></td>
+            <!-- <td><a href="delete.php?deleteID=<?php echo $stdID ?>">Delete</a></td> -->
+            <!-- use double quotes for js inside php! -->
+            <td><a onClick="javascript: return confirm('Please confirm deletion')" href="delete.php?deleteID=<?php echo $stdID ?>">Delete</a></td>
+
+            <td><a href="submitmore.php?submitID=<?php echo $stdID ?>">Submit more</a></td>
+            <td><a href="openfolder.php?openFolderID=<?php echo $stdLName . '_' . $stdFName ?>">View in folder</a></td>
+            <!-- open a new browser tab to view the student profile -->
+            <td><a href="profile.php?viewProfileID=<?php echo $stdID ?>" target="_blank">View profile</a></td>
+          </tr>
+          <?php
+        } // end of while
         ?>
     </table>
   </body>
