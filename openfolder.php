4c40a602128934771c409f8867fca2e53173032f
vhphong <vhphong@gmail.com>
2020-11-13 14:02:24 -0500

    fixed: openFolderID var to open folder

--- openfolder.php
+++ openfolder.php
@@ -1,12 +1,28 @@
 <?php
   require_once("connection.php");
-?>
+
+  // retrieve Student ID whose information to be edited
+  $stdID = $_GET['openFolderID'];
+  $queryOpenFolder = "SELECT * FROM studentinfo WHERE sid = '".$stdID."' ";
+  $resultOpenFolder = mysqli_query($dbconnection, $queryOpenFolder);
+
+  while ($row=mysqli_fetch_assoc($resultOpenFolder)) {
+    $stdID = $row['sid'];
+    $stdFName = $row['firstname'];
+    $stdLName = $row['lastname'];
+    $stdSSN = $row['ssn'];
+    $stdDOB = $row['dob'];
+    $stdGender = $row['gender'];
+    $stdRace = $row['race'];
+    $stdAvatar = $row['photo'];
+  }
+ ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Open folder</title>
+    <title>Student's folder</title>
   </head>
   <body>
     <h4>openfolder.php</h4>
@@ -15,18 +31,17 @@
       // if the "View in folder" button was pressed
       if (isset($_GET['openFolderID']))
       {
-        $studentFolder = $_GET['openFolderID'];
         // Opening a directory
         // absolute path
         // $path = "C:/xampp/htdocs/PHP_Projects/studentmanager/" . $studentFolder;
         // relative path
-        $path = ".\\" . $studentFolder;
+        $studentFolder = $stdLName . '_' . $stdFName;
 
-        if (!is_dir($path)) {
+        if (!is_dir($studentFolder)) {
           echo '<script>alert("Folder of student is not found.")</script>';
         }
         else {
-          exec("EXPLORER /E, $path");
+          exec("EXPLORER /E, $studentFolder");
         }
       }
     ?>
