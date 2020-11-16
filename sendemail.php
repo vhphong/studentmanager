287baf6a93c1d9c08bd1a6a2449ed0e9f85c4af6
vhphong <vhphong@gmail.com>
2020-11-16 15:01:26 -0500

    added Form Handling: fields requirements

--- sendemail.php
+++ sendemail.php
@@ -1,7 +1,63 @@
-<!-- raw -->
-
-
 <?php
+  require_once("connection.php");
+
+  // retrieve Student ID whose information to be edited
+  $stdID = $_GET['emailID'];
+  $querySendEmail = "SELECT * FROM studentinfo WHERE sid = '".$stdID."' ";
+  $resultSendEmail = mysqli_query($dbconnection, $querySendEmail);
+
+  while ($row=mysqli_fetch_assoc($resultSendEmail)) {
+    $stdID = $row['sid'];
+    $stdFName = $row['firstname'];
+    $stdLName = $row['lastname'];
+    $stdSSN = $row['ssn'];
+    $stdDOB = $row['dob'];
+    $stdGender = $row['gender'];
+    $stdRace = $row['race'];
+    $stdAvatar = $row['photo'];
+  }
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Send Email</title>
+  </head>
+  <body>
+    <h4>sendemail.php</h4>
+    <h2>Student's Profile</h2>
+    <a href="index.php">Back to Main Data Entry Form</a>
+    <br>
+    <a href="view.php">Back to View Students Data</a>
+    <br><br>
+
+    <?php
+
+
+
+    ?>
+    <textarea name="comment" rows="5" cols="40"></textarea>
+    <br><br>
+    <button type="button" name="button">Send Email</button>
+
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- raw -->
+<!-- <?php
     $to      = "abc@example.com";
     $subject = "Subject of the email";
     $message = "Content of the email";
@@ -16,4 +72,4 @@
     else {
       echo "Email was not sent...";
     }
-?>
+?> -->
