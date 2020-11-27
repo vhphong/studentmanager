f27e618dc42dd4ac743749641cf959dfb135bd5c
vhphong <vhphong@gmail.com>
2020-11-27 11:56:08 -0500

    fixed queryUpdate

--- update.php
+++ update.php
@@ -26,9 +26,9 @@
                         , dob = '".$stdDOB."'
                         , gender = '".$stdGender."'
                         , race = '".$stdRace."'
-                        -- , photo = '".$stdAvatar."'
-                        , submission = '".$stdSubmissions."'
                     WHERE sid = '".$stdID."' ";
+                        // , photo = '".$stdAvatar."'
+                        // , submission = '".$stdSubmissions."'
 
     // test queryUpdate
     // $queryUpdate = "SELECT * FROM studentinfo";
@@ -44,69 +44,9 @@
       echo "Update failed. Please check queryUpdate";
     }
   }
-  // if UPDATE button was not pressed
-  else {
-    header("location:view.php");
-  }
 
-?>
-
-  }
-
-?>
-lder = $stdLName . '_' . $stdFName;
-
-    if (!is_dir($studentFolder)){
-      mkdir($studentFolder, 0777, true);
-    }
-
-    // rename file into Last_First.jpg/png/bmp pattern
-    $avatarPath     = $studentFolder . "/" . $newAvatarFileName;
-    $submissionPath = $studentFolder . "/" . $submissionName;
-
-    if (!in_array($avatarExtension, $allowed_avatar_file_types) || ($avatarSize > 5000000)) {
-      // echo "You file extension must be .jpg, .png or .bmp";
-      header("location:failed.php");
-    }
-    else {
-      $queryUpdate = "UPDATE studentinfo
-                      SET firstname = '".$stdFName."'
-                        , lastname = '".$stdLName."'
-                        , ssn = '".$stdSSN."'
-                        , dob = '".$stdDOB."'
-                        , gender = '".$stdGender."'
-                        , race = '".$stdRace."'
-                        , photo = '".$newAvatarFileName."'
-                        , submission = '".$stdSubmissions."'
-                      WHERE sid = '".$stdID."' ";
-    }
-
-    // test queryUpdate
-    // $queryUpdate = "SELECT * FROM studentinfo";
-
-    // $resultUpdate = mysqli_query($dbconnection, $queryUpdate);
-
-    if (mysqli_query($dbconnection, $queryUpdate))
-    {
-      // echo "Updated successfully";
-      header("location:view.php");
-    }
-    else {
-      // echo "Update failed. Please check queryUpdate";
-      header("location:view.php");
-    }
-
-    if (move_uploaded_file($avatarTemp, $avatarPath)) {
-      header("location:view.php");
-    }
-    else {
-      header("location:failed.php");
-    }
-
-
-  }
-  // if UPDATE button was not pressed
-  else {
+  // if "VIEW RECORDS" button was pressed
+  if (isset($_POST['display'])) {
     header("location:view.php");
   }
 
